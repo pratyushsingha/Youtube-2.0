1606364623f0c052896d79882be6904c547b3ec3
pratyush Singha <pratyushsingha83@gmail.com>
2023-08-31 01:22:16 +0530

    some bugs removing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,18 +8,19 @@ const Sidebar = () => {
     const { tagsVideos } = useContext(AppContext);
 
     return (
-        <div className='hidden md:block ml-1 mr-3 mt-3'>
-            {categories.map((category, index) => {
-                return (
-                    <button onClick={() => tagsVideos(category.name)} key={index} className={`flex items-center rounded-xl justify-center bg-white-500 text-black py-3 px-6 font-semibold font-sarif hover:bg-red-600 hover:text-white cursor-pointer`}>
-                        {category.name}
-                    </button>
-                )
-            })
-            }
+        <>
 
-        </div>
-
+            <div className='hidden md:block ml-1 mr-3 mt-3'>
+                {categories.map((category, index) => {
+                    return (
+                        <button onClick={() => tagsVideos(category.name)} key={index} className={`flex items-center rounded-xl justify-center bg-white-500 text-black py-3 px-6 font-semibold font-sarif hover:bg-red-600 hover:text-white cursor-pointer`}>
+                            {category.name}
+                        </button>
+                    )
+                })
+                }
+            </div>
+        </>
     )
 }
 
